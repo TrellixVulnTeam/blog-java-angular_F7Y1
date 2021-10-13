04992324bbbb8de4dd5447ebcc2ce87e7792cdf0
Nanderson Rodrigues <nandersondsr@gmail.com>
2021-10-13 12:47:22 -0300

    Add methods to consume api
    - method to return posts (get)
    - method to create a post (post)

--- frontend/blog/src/app/feed/feed.component.ts
+++ frontend/blog/src/app/feed/feed.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from '../service/post.service';
+import { Post } from '../model/Post';
 
 @Component({
   selector: 'app-feed',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedComponent implements OnInit {
 
-  constructor() { }
+  listPost: Post[] = [];
+  post: Post = new Post;
+
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
+    this.findPosts();
+  }
+
+  findPosts () {
+    this.postService.getPost().subscribe((data: any) => {
+      this.listPost = data;
+    })
+  }
+
+  createPost () {
+    this.postService.postPost(this.post).subscribe((data: any)=>{
+      this.post = data,
+      location.assign("/feed")
+    })
   }
 
 }
